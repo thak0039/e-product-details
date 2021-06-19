7cfc35be6e42fbec358073e0626cb01c55d9a939
thak0039 <thak0039@algonquinlive.com>
2021-06-19 01:48:11 -0400

    have add the lable of product and price with javascript

--- main.js
+++ main.js
@@ -1,11 +1,27 @@
-// let minImg = document.querySelector(".product-img-container img");
-
-const { Transform } = require("node:stream");
-
-// let bigImg = document.querySelector(".main-product-container img");
-// minImg.addEventListener("mousehover", GetmainproductImg(this.src));
-
 function GetmainproductImg(src) {
     document.querySelector(".main-product-container img").src = src;
+
+}
+
+
+function showValueOnSide() {
+    let inputprice = document.getElementById('price').value;
+    document.getElementById('selectprice').innerHTML = inputprice;
+
+    let inputQat = document.getElementById('qat').value;
+    document.getElementById('selectqat').innerHTML = inputQat;
+
+    let inputDiscount = document.getElementById('discount').value;
+    let priPluseQat = parseInt(inputprice * inputQat);
+    let per = priPluseQat * inputDiscount / 100;
+    document.getElementById('selectdiscount').innerHTML = inputDiscount + "%" + " " + " Discounted Price: " + per;
+    subtotal = priPluseQat - per;
+    document.getElementById('subtotatal').innerHTML = subtotal;
+    prodTax = subtotal * 13 / 100;
+
+    pluseProdtax = subtotal + prodTax;
+    document.getElementById('total-price').innerHTML = pluseProdtax;
+    // console.log(subtotal);
+
 
 }
